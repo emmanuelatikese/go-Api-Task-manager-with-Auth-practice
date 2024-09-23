301f0caeb92f0a9edef728229dfaba31dee9dc02
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-09-23 11:17:25 +0100

    adding files

--- controllers/userController.go
+++ controllers/userController.go
@@ -2,7 +2,9 @@ package controllers
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
+
 	"api.task/db"
 	jwtFunc "api.task/jwt"
 	"api.task/models"
@@ -13,22 +15,38 @@ import (
 
 func SignUp (w http.ResponseWriter, r *http.Request) {
 	var userModel routes.UserModel
+	userMap := make(map[string]string)
 	mongDb := apiDB.MongoDB
 	userCollection := mongDb.Collection("userCollection")
 	ctx := apiDB.Ctx
 
-	err := json.NewDecoder(r.Body).Decode(&userModel)
+	err := json.NewDecoder(r.Body).Decode(&userMap)
 	if err != nil {
 		apiUtils.JsonResponse(err.Error(), w, http.StatusBadRequest)
+		return
 	}
-
-	if userModel.Username == "" || string(userModel.Password) != string(userModel.Confirm_Password) {
+	var commonUser routes.UserModel
+	err = userCollection.FindOne(ctx, bson.M{"username": userModel.Username}).Decode(&commonUser)
+	if err != nil{
+		apiUtils.JsonResponse(err, w, 500)
+		return
+	}
+	
+	log.Print(userMap["username"])
+	if userMap["username"] == "" || userMap["password"] != userMap["confirmed_password"] {
 		apiUtils.JsonResponse("Username invalid or Passwords mismatch", w, http.StatusNotAcceptable)
 		return
 	}
-	if len(string(userModel.Password)) < 6 {
+	if len(userMap["password"]) < 6 {
+		log.Print(len(userMap["password"]))
 		apiUtils.JsonResponse("Password must be 6 or more", w, http.StatusNotAcceptable)
 		return
+	}
+
+	userModel = routes.UserModel{
+		Username: userMap["username"],
+		Email: userMap["email"],
+		Password: []byte(userMap["password"]),
 	}
 
 	HashPassword, err := bcrypt.GenerateFromPassword(userModel.Password, bcrypt.DefaultCost)
@@ -36,13 +54,10 @@ func SignUp (w http.ResponseWriter, r *http.Request) {
 		apiUtils.JsonResponse(err.Error(), w, 500)
 		return
 	}
+	userModel.Password = HashPassword
 
 
-	insertId, err := userCollection.InsertOne(ctx, bson.M{
-		"username": userModel.Username,
-		"email": userModel.Email,
-		"password": HashPassword,
-	})
+	insertId, err := userCollection.InsertOne(ctx, userModel)
 	if err != nil {
 		apiUtils.JsonResponse(err, w, 500)
 	}
@@ -51,7 +66,7 @@ func SignUp (w http.ResponseWriter, r *http.Request) {
 	response := map[string]interface{}{
 		"_id": insertId.InsertedID,
 		"username": userModel.Username,
-		"Email": userModel.Email,
+		"email": userModel.Email,
 	}
 
 	apiUtils.JsonResponse(response, w, 200)
--- models/userModels.go
+++ models/userModels.go
@@ -4,6 +4,4 @@ type UserModel struct {
 	Username string `json:"username"`
 	Email string `json:"email"`
 	Password []byte `json:"password"`
-	Confirm_Password []byte `json:"confirmed_password"`
-
 }
